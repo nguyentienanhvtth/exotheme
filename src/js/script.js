f7625397d39133f4c7187ae906d226521bd4b48a
TrangSulli <trangmap555@gmail.com>
2018-05-11 02:07:24 +0700

    chua responsive

--- src/js/script.js
+++ src/js/script.js
@@ -14,4 +14,30 @@ jQuery(document).ready(function(){
   $(window).on('load', function () {
 		$('.ma5slider').ma5slider();
  });
+
 });
+$(document).ready(function(){
+
+	//Check to see if the window is top if not then display button
+		$(window).scroll(function(){
+			if ($(this).scrollTop() > 400) {
+					$('.button-scroll').fadeIn();
+			} else {
+					$('.button-scroll').fadeOut();
+			}
+	});
+
+	//Click event to scroll to top
+	$('.button-scroll').click(function(){
+			$('html, body').animate({scrollTop : 0},800);
+			return false;
+	});
+						 
+
+});
+jQuery(document).ready(function(){
+  
+	new WOW().init();
+    
+});
+
